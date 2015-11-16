bba2c0fd212e85b4caef1d7ac6d46452475bc5ce
joe2mcdonald <ns7@san.rr.com>
2015-11-16 11:50:27 -0800

    Assignment8

--- calculator/js/calc.js
+++ calculator/js/calc.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){            
 // DOCUMENT IS READY
 	
-$(".number,.symbol").click(function(){   // IF (+ - * /) SYMBOL IS CLICKED
+$(".num,.opr").click(function(){         // IF NUMBER OR OPERATOR (+ - * /) IS CLICKED
 var value = $(this).val();               // THIS GETS VALUE FROM RESULT TEXTBOX
 $('#resultId').val($('#resultId').val() + value);  // ADDED TO THE RESULT TEXTBOX
 });
 	
 //  CLEAR  VALUES 	
-$('#clearId').click(function(){           // IF CLEAR IS CLICKED
+$('#clearId').click(function(){          // IF CLEAR IS CLICKED
 $('#resultId').val('');
 $('#operId').val('');
 $('#operId').removeClass('activeAnswer');  // REMOVE ACTIVEANSWER CLASS FROM OPERATION 
@@ -16,9 +16,9 @@ $("#resultId").css({"background-color": "#f1ff92" });
 });
 	
 //  CALCULATION	OF RESULT TEXT 
-$('#equalsId').click(function(){            // IF EQUAL IS CLICKED
-var calc = $('#resultId').val();            // ASSIGNS THE VARIABLE WITH THE RESULT DATA
-$('#resultId').val(eval(calc));             // EVAL CALCULATIO
+$('#equalsId').click(function(){        // IF EQUAL IS CLICKED
+var calc = $('#resultId').val();        // ASSIGNS THE VARIABLE WITH THE RESULT DATA
+$('#resultId').val(eval(calc));         // EVAL CALCULATIO
 $("#resultId").css({"background-color": "#92eaff" });
 });
 });  // END OF DOCUMENT READY
